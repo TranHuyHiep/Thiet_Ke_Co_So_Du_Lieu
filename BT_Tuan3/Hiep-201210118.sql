79cb368cd34e5da5a271ecf2f18a39bffee34c6c
TranHuyHiep <tranhuyhiep0710@gmail.com>
2022-10-04 07:13:29 +0700

    cau 6

--- BT_Tuan3/Hiep-201210118.sql
+++ BT_Tuan3/Hiep-201210118.sql
@@ -24,43 +24,62 @@ select MaSV, HoSV, TenSV, TenKhoa, Phai
 from DSSinhVien join DMKhoa on DSSinhVien.MaKhoa = DMKhoa.MaKhoa
 where (TenKhoa = N'Tin Học' or TenKhoa = N'Anh Văn') and Phai = N'Nam'
 
-select *
-from Cau3
+SELECT *
+FROM Cau3
 -- Câu 4: Tạo view gồm những sinh viên có tuổi từ 20 đến 25, thông tin gồm: Họ tên sinh viên,
 -- Tuổi, Tên khoa.
-create view Cau4 as 
-select HoSV, TenSV, convert(int, DATEDIFF(DD, NgaySinh, getDate())/365.25) as tuoi, TenKhoa
-from DSSinhVien join DMKhoa on DSSinhVien.MaKhoa = DMKhoa.MaKhoa
-where DATEDIFF(DD, NgaySinh, getDate())/365.25 >= 20 
-	and DATEDIFF(DD, NgaySinh, getDate())/365.25 <= 25
+CREATE VIEW Cau4 AS 
+SELECT HoSV, TenSV, CONVERT(INT, DATEDIFF(DD, NgaySinh, GETDATE())/365.25) AS tuoi, TenKhoa
+FROM DSSinhVien JOIN DMKhoa ON DSSinhVien.MaKhoa = DMKhoa.MaKhoa
+WHERE DATEDIFF(DD, NgaySinh, GETDATE())/365.25 >= 20 
+	AND DATEDIFF(DD, NgaySinh, GETDATE())/365.25 <= 25
 
-select * from Cau4
+SELECT * FROM Cau4
 -- Câu 5: Tạo view cho biết thông tin về mức học bổng của các sinh viên, gồm: 
 -- Mã sinh viên, Phái, Mã khoa, Mức học bổng. 
 -- Trong đó, mức học bổng sẽ hiển thị là “Học bổng cao”
 -- nếu giá trị của field học bổng lớn hơn 500,000 và ngược lại hiển thị là “Mức trung bình”
-create view Cau5 as
-select MaSV, Phai, MaKhoa, 
+CREATE VIEW Cau5 AS
+SELECT MaSV, Phai, MaKhoa, 
 (
-	case 
-	when HocBong > 500000 then N'Học bổng cao'
-	else N'Mức trung bình'
-	end
-) as MucHocBong
-from DSSinhVien
-where HocBong > 0
+	CASE 
+	WHEN HocBong > 500000 THEN N'Học bổng cao'
+	ELSE N'Mức trung bình'
+	END
+) AS MucHocBong
+FROM DSSinhVien
+WHERE HocBong > 0
 
-select * from Cau5
+SELECT * FROM Cau5
 
 -- Câu 6: Tạo view đưa ra thông tin những sinh viên có học bổng lớn hơn bất kỳ học bổng của
 -- sinh viên học khóa anh văn
-create view Cau6 as
-select MaSV, HoSV, TenSV, HocBong
-from DSSinhVien inner join DMKhoa on DSSinhVien.MaKhoa = DMKhoa.MaKhoa
-where hocbong > (select top (1) HocBong
-					from DSSinhVien join DMKhoa on DSSinhVien.MaKhoa = DMKhoa.MaKhoa
-					where TenKhoa = N'Anh Văn' 
-					order by HocBong desc)
-go
-select * from Cau6
+CREATE VIEW Cau6 AS
+SELECT MaSV, HoSV, TenSV, HocBong
+FROM DSSinhVien INNER JOIN DMKhoa ON DSSinhVien.MaKhoa = DMKhoa.MaKhoa
+WHERE hocbong > (SELECT TOP (1) HocBong
+					FROM DSSinhVien JOIN DMKhoa ON DSSinhVien.MaKhoa = DMKhoa.MaKhoa
+					WHERE TenKhoa = N'Anh Văn' 
+					ORDER BY HocBong DESC)
+GO
+SELECT * FROM Cau6
+
+-- Câu 7: Tạo view đưa ra thông tin những sinh viên đạt điểm cao nhất trong từng môn. (chua xong)
+SELECT DMMonHoc.MaMH, TenMH, t.HoSV, t.TenSV, t.Diem
+FROM DMMonHoc JOIN (SELECT DSSinhVien.MaSV, HoSV, TenSV, Phai, NgaySinh, NoiSinh, Diem, MaMH
+					FROM KetQua JOIN DSSinhVien ON KetQua.MaSV = DSSinhVien.MaSV
+					ORDER BY Diem DESC) AS t ON t.MaMH = DMMonHoc.MaMH
+
+
+SELECT DSSinhVien.MaSV, HoSV, TenSV, Phai, NgaySinh, NoiSinh, Diem, MaMH
+FROM KetQua JOIN DSSinhVien ON KetQua.MaSV = DSSinhVien.MaSV
+ORDER BY Diem DESC
+
+SELECT *
+FROM dbo.DMMonHoc
+
+-- Câu 8: Tạo view đưa ra những sinh viên chưa thi môn cơ sở dữ liệu.
+
+-- Câu 9: Tạo view đưa ra thông tin những sinh viên không trượt môn nào.
+
 		
